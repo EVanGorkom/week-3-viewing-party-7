d7df71fbc8648b342128d754ce30e4630eae134c
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-24 18:39:30 -0500

    Finish the functionality of User Story 2

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -29,4 +29,33 @@ RSpec.describe "User Registration" do
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
   end
+
+  it 'if I fail to fill in my name, a unique email, or matching passwords, Im returned to the /register page' do
+    User.create(name: 'User One', password: 'p@ssw0rd', email: 'notunique@example.com')
+    visit register_path
+
+    # name test
+    fill_in :user_name, with: ''
+    fill_in :user_password, with: 'p@ssw0rd'
+    fill_in :user_password_confirmation, with: 'p@ssw0rd'
+    fill_in :user_email, with:'unique@example.com'
+    click_button 'Create New User'
+    expect(current_path).to eq(register_path)
+
+    # unique email test
+    fill_in :user_name, with: 'Ernie'
+    fill_in :user_password, with: 'p@ssw0rd'
+    fill_in :user_password_confirmation, with: 'p@ssw0rd'
+    fill_in :user_email, with:'notunique@example.com'
+    click_button 'Create New User'
+    expect(current_path).to eq(register_path)
+
+    # matching passwords test
+    fill_in :user_name, with: 'Ernie'
+    fill_in :user_password, with: 'p@ssw0rd'
+    fill_in :user_password_confirmation, with: 'wordpass'
+    fill_in :user_email, with:'unique@example.com'
+    click_button 'Create New User'
+    expect(current_path).to eq(register_path)
+  end
 end
